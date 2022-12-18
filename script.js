fc2b08504de0174f659be1e4d36264a93db6e767
Darylcosm0 <mutaidaryl@gmail.com>
2022-12-18 16:15:42 +0300

    add js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//DOM Vars
 const iconToggle = document.getElementById('toggle-icon');
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 
@@ -9,3 +9,37 @@ const elmntColor = '--elements-color';
 const inptColor = '--input-color';
 const boxShadow = '--box-shadow';
 let darkMode = true;
+
+
+const refresh =  () =>  {    
+    setTimeout(() => {
+        window.location.reload();
+    }, 50);
+}
+
+const switchToLightMode  = () => {
+    document.documentElement.style.setProperty(bgColor, 'hsl(0, 0%, 98%)');
+    document.documentElement.style.setProperty(txtColor, 'hsl(200, 15%, 8%)');
+    document.documentElement.style.setProperty(elmntColor, 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty(inptColor, 'hsl(0, 0%, 52%)');
+    document.documentElement.style.setProperty(boxShadow,'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;');
+    iconToggle.classList.remove('fas');
+    iconToggle.classList.add('far');
+    localStorage.setItem('isDark', 'no');
+    darkMode = false;
+ 
+};
+
+const switchToDarkMode  = () => {
+    document.documentElement.style.setProperty(bgColor, 'hsl(207, 26%, 17%)');
+    document.documentElement.style.setProperty(txtColor, 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty(elmntColor, 'hsl(209, 23%, 22%)');
+    document.documentElement.style.setProperty(inptColor, 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty(boxShadow,'none');
+
+    iconToggle.classList.remove('far');
+    iconToggle.classList.add('fas');
+    localStorage.setItem('isDark', 'yes');
+    darkMode = true;
+ 
+};
