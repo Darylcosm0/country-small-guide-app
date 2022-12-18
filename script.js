f96775d9a35ba5a1059a2cfa31e04ccd98b8763a
Darylcosm0 <mutaidaryl@gmail.com>
2022-12-18 16:10:09 +0300

    add js dom variable and themes

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const iconToggle = document.getElementById('toggle-icon');
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+//Theme Vars
+const bgColor = '--bg-color';
+const txtColor = '--text-color';
+const elmntColor = '--elements-color';
+const inptColor = '--input-color';
+const boxShadow = '--box-shadow';
+let darkMode = true;
